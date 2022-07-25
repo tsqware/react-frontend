3a0933d87802a33c17df90ad9d8fe96a6821fa82
Terrence Tyson <tsqware@gmail.com>
2022-07-25 03:45:06 -0400

    refactored in http-hook

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -9,12 +9,12 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 
 import './Auth.css';
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Auth = () => {
 	const auth = useContext(AuthContext);
 	const [isLoginMode, setIsLoginMode] = useState(true);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState()
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
 	const [formState, inputHandler, setFormData] = useForm({
 		email: {
@@ -32,7 +32,6 @@ const Auth = () => {
 
 	const switchModeHandler = () => {
 		if (!isLoginMode) {
-			setIsLoading(true);
 			setFormData(
 				{
 					...formState.inputs,
@@ -54,76 +53,42 @@ const Auth = () => {
 
 	const authSubmitHandler = async event => {
 		event.preventDefault();
-		setIsLoading(true);
 
 		if (isLoginMode) {
+
 			try {
-				const response = await fetch('http://localhost:5001/api/users/login', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
+				await sendRequest(
+					'http://localhost:5001/api/users/login',
+					'POST',
+					JSON.stringify({
 						email: formState.inputs.email.value,
 						password: formState.inputs.password.value
-					})
-				});
-
-
-
-				setIsLoading(false);
+					}),
+					{ 'Content-Type': 'application/json' },
+				);
 				auth.login();
-			} catch (err) {
-				console.log("authSubmit err:", err);
-				setIsLoading(false);
-				setError(err.message || 'Something went wrong when trying to log in.');
-				return;
-			}
+			} catch (err) { }
 		} else {
 			try {
-				const response = await fetch('http://localhost:5001/api/users/signup', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
+				const response = await sendRequest(
+					'http://localhost:5001/api/users/signup',
+					'POST',
+					JSON.stringify({
 						name: formState.inputs.name.value,
 						email: formState.inputs.email.value,
 						password: formState.inputs.password.value,
 						image: "http://localhost:3000/images/blank-profile-picture-973460_640.png"
-					})
-				});
-
-				const responseData = await response.json();
-
-				if (!response.ok) {
-					throw new Error(responseData.message);
-				}
-
-				setIsLoading(false);
+					}),
+					{ 'Content-Type': 'application/json' }
+				);
 				auth.login();
-			} catch (err) {
-				console.log("authSubmit err:", err);
-				setIsLoading(false);
-				setError(err.message || 'Something went wrong when trying to sign up.');
-				return;
-			}
+			} catch (err) { }
 		}
-
-		setIsLoading(false);
-
-
-
-		auth.login();
-	}
-
-	const errorHandler = () => {
-		setError(null)
-	}
+	};
 
 	return (
 		<>
-			<ErrorModal error={error} onClear={errorHandler} />
+			<ErrorModal error={error} onClear={clearError} />
 			<Card className="authentication">
 				{isLoading && <LoadingSpinner asOverlay />}
 				<h2>Login</h2>
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,41 +2,27 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Users = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState();
+	const { isLoading, error, sendRequest, clearError } = useHttpClient();
 	const [loadedUsers, setLoadedUsers] = useState();
 
 	useEffect(() => {
-		const sendRequest = async () => {
-			setIsLoading(true);
+		const fetchUsers = async () => {
 			try {
-				const response = await fetch('http://localhost:5001/api/users');
-				const responseData = await response.json();
-
-				if (!response.ok) {
-					throw new Error(responseData.message);
-				}
-
+				const responseData = await sendRequest('http://localhost:5001/api/users');
 				setLoadedUsers(responseData.users);
-			} catch (err) {
-
-				setError(err.message)
-			}
-			setIsLoading(false);
-
+			} catch (err) { }
 		};
-		sendRequest();
-	}, []);
+		fetchUsers();
+	}, [sendRequest]);
 
-	const errorHandler = () => {
-		setError(null);
-	}
+
 
 	return (
 		<>
-			<ErrorModal error={error} onClear={errorHandler} />
+			<ErrorModal error={error} onClear={clearError} />
 			{isLoading && <div className="center"><LoadingSpinner /></div>}
 			{!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
 		</>
