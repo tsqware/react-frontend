84e88812299584a95e570ffe3a821d2107d1865a
Terrence Tyson <tsqware@gmail.com>
2022-07-30 21:00:35 -0400

    added lazy routes, suspense

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import React from "react";
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
+import React, { Suspense } from "react";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
+const Auth = React.lazy(() => import('./user/pages/Auth'));
+
 
 
 const App = () => {
@@ -49,9 +52,9 @@ const App = () => {
 			<Router>
 				<MainNavigation />
 				<main>
-					<Routes>
-						{routes}
-					</Routes>
+					<Suspense fallback={<div className="center"><LoadingSpinner /></div>}>
+						<Routes>{routes}</Routes>
+					</Suspense>
 				</main>
 			</Router>
 		</AuthContext.Provider>
